5fc6066ca0d38299ba469f023897c370a1f665cc
Pavlo Smolyansky <pavlo.smolyansky@aspose.com>
2023-05-23 16:12:03 +0200

    Added some configurations for Comparison UI controller

---
+++ src/GroupDocs.Comparison.UI.Api/Entity/FileDescriptionEntity.cs
@@ -0,0 +1,16 @@
+﻿
+namespace GroupDocs.Comparison.UI.Api.Entity
+{
+
+    /// <summary>
+    /// File description entity
+    /// </summary>
+    public class FileDescriptionEntity
+    {
+        public string guid { get; set; }
+        public string name { get; set; }
+        public string docType { get; set; }
+        public bool isDirectory { get; set; }
+        public long size { get; set; }
+    }
+}
---
+++ src/GroupDocs.Comparison.UI.SelfHost.Api/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using GroupDocs.Comparison.UI.SelfHost.Api;
+using GroupDocs.Comparison.UI.SelfHost.Api.Configuration;
+
+namespace Microsoft.Extensions.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+        public static Config BindSelfHostApiSettings
+            (this IConfiguration configuration, Config config)
+        {
+            configuration
+                .GetSection(Keys.GROUPDOCSCOMPARISONUI_SELF_HOST_API_SECTION_SETTING_KEY)
+                .Bind(config, c => c.BindNonPublicProperties = true);
+
+            return config;
+        }
+    }
+}
